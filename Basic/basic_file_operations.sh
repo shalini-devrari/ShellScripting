92d1f2d14e622f4f0ed407902013c1a47d6d633f
shalini devrari <devrarishalini144@gmail.com>
2024-08-22 11:05:07 +0530

    Add script to perform basic file operations

---
+++ Basic/basic_file_operations.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Create a new directory
+mkdir my_directory
+
+# Create a new file
+touch my_directory/my_file.txt
+
+# Rename the file
+mv my_file.txt renamed_file.txt
+
+# Remove the file
+rm renamed_file.txt
+
+# Remove the directory
+ rmdir my_directory
+
+echo "Basic file operations completed."
