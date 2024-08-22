bc985f4d961775c0a6eb775e66824a03fbe3d08f
shalini devrari <devrarishalini144@gmail.com>
2024-08-22 11:25:18 +0530

    Add script to navigate directories

---
+++ Basic/navigate_directories.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+#Navigate to the home directory
+cd
+
+#to navigate one directory back
+cd ..
+
+#to navigate two directory back
+cd ../..
+
+# Navigate to the root directory
+cd /
+
+# Print the current directory
+pwd
+
+echo "Directory navigation completed."
