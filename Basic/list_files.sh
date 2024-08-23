91e7f1a2b29051cb1133f4e4e7dbe97b43ab4f08
shalini devrari <devrarishalini144@gmail.com>
2024-08-23 10:12:58 +0530

    Add new lines

--- Basic/list_files.sh
+++ Basic/list_files.sh
@@ -5,24 +5,31 @@ ls
 # Show files with detailed information, including permissions and sizes
 echo "Detailed listing including permissions and sizes:"
 ls -l
+
 #Show all files including hidden files
 echo "Include hidden files (dotfiles):"
 ls -a
+
 # Long listing format including hidden files
 echo "Include hidden files (dotfiles):"
 ls -la
+
 # List files in a human-readable format
 echo "Human-readable file sizes (e.g., KB, MB)"
 ls -lh
+
 # List files with inode numbers
 echo "List files with inode numbers:"
 ls -i
+
 # Sort files by modification time
 echo "Sort files by modification time:"
 ls -lt
+
 # Reverse order (reverse the sorting)
 echo "Reverse the sorting order:"
 ls -lr
+
 # List files recursively (including subdirectories)
 echo Recursive listing:""
 ls -R
